956326a84055a8a51d19e19f3538bc50a94cfb05
David Jones <djones6dev@gmail.com>
2019-04-05 15:10:25 +0100

    Support Swift 5 (#56)
    * ci: Update to Swift 5.0, use official Docker images
    
    * Update project to Swift 5

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,7 +1,7 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 /*
- * Copyright IBM Corporation 2017-2019
+ * Copyright IBM Corporation 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
