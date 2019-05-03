d9e3ae3b4822b7d57b4bae8407049de8d8c118b8
David Jones <djones6@uk.ibm.com>
2019-05-03 16:26:46 +0100

    Add a Kitura-based test server for SwiftyRequest

---
+++ TestServer/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TestServer",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.6.0"),
+        .package(url: "https://github.com/IBM-Swift/FileKit.git", .upToNextMinor(from: "0.0.0")),
+        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.8.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "TestServer",
+            dependencies: ["Kitura", "FileKit", "HeliumLogger"]),
+    ]
+)
