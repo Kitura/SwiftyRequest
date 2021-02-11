694959ab8ad12766c4d5141260b3775a28d2e56f
Danny Sung <danny_sung@intuit.com>
2021-02-11 06:54:48 -0800

    re-introduce shutdown fix and bump to async-http-client 1.2.0

--- Package.swift
+++ Package.swift
@@ -29,8 +29,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Kitura/CircuitBreaker.git", from: "5.0.200"),
         .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200"),
-        //.package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMinor(from: "1.1.0")),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
     ],
     targets: [
         .target(
